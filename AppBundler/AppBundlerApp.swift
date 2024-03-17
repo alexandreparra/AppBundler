357b84555c2de9feddf442f662b45907c8989dbf
Alexandre Parra <duffhd@outlook.com>
2024-03-17 08:17:59 -0300

    Initial Commit

---
+++ AppBundler/AppBundlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppBundlerApp.swift
+//  AppBundler
+//
+//  Created by Ale on 17/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AppBundlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
